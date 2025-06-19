4fe2c0bae3d4396fcda506fc885e69e670a60812
soumo <soumojitmakar1234@gmail.com>
2025-06-19 06:05:33 +0530

    add frontend

---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,19 @@
+function Home() {
+    return (
+        <div className="flex w-full  min-h-screen justify-center items-center  ">
+            <div className="w-1/2 text-center">
+                <h1 className="text-lg text-balance">👋 Hey there, welcome to Twitter Automation AI!</h1>
+                <br />
+                <p className="text-gray-500">We’re super excited to have you join us.
+                    Ready to save time, post smarter, and grow faster? Let the AI take care of your tweets while you focus on what matters most.
+
+                    Let’s get tweeting! 🐦💬🚀</p>
+                <form action=" " className="flex justify-around" >
+                    <input type="text" className="border-2 w-4/5 focus:border-2 focus:border-blue-500" />
+                    <button className=" p-2 bg-gray-600 rounded-xl"><svg xmlns="http://www.w3.org/2000/svg" height={"20px"} width={"20px"}  viewBox="0 0 512 512"><path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480l0-83.6c0-4 1.5-7.8 4.2-10.8L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z" /></svg></button>
+                </form>
+            </div>
+        </div>
+    )
+}
+export default Home
