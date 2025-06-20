cf6c5e8b1ac887fc376c425d0d963e6678c817d8
soumo <soumojitmakar1234@gmail.com>
2025-06-20 10:34:29 +0530

    customize For Prod

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -3,21 +3,22 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.routes.twitter_routes import router as TwitterRoutes
 from contextlib import asynccontextmanager
 from src.db import create_table
+from src.config import FRONTEND_URL
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_table()
     yield
 app = FastAPI(
-    title=" My App",
+    title=" Twitter Automation API",
     version="1.0.0",
-    description="",
+    description="An API that generates, stores, and posts AI-powered tweets.",
     lifespan=lifespan
 )
 
-origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
